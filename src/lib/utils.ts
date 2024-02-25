95aa8d6cd0d9902e54387e04a1a779e8e7773f4b
myjhye <t01087633799@gmail.com>
2024-02-25 23:16:48 +0900

    카트

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,29 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "BDT" | "KRW",
+    notation?: Intl.NumberFormatOptions["notation"]
+  } = {}
+) {
+
+  // 기본으로 'USD', 'compact' 사용
+  const { 
+    currency = "KRW",  
+    notation = "standard" 
+  } = options
+
+  // price가 문자열이면 숫자로 변환
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price
+
+  // 제공된 옵션에 따라 숫자 가격 형식화 - Intl.NumberFormat 사용
+  return new Intl.NumberFormat("ko-KR", {
+    style: "currency",
+    currency: currency,
+    notation: notation,
+    maximumFractionDigits: 0,
+  }).format(numericPrice)
+}
