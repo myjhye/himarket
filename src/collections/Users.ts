914c649a19715dc36a37dbca49187a34363373fd
myjhye <t01087633799@gmail.com>
2024-03-02 04:14:32 +0900

    user 데이터 스키마

---
+++ src/collections/Users.ts
@@ -0,0 +1,31 @@
+// Users 데이터 스키마
+
+import { CollectionConfig } from "payload/types";
+
+export const Users: CollectionConfig = {
+    slug: "users",
+    auth: true,
+    access: {
+        read: () => true,
+        create: () => true,
+    },
+    fields: [
+        {
+            name: 'role',
+            defaultValue: 'user',
+            required: true,
+            type: 'select',
+            options: [
+                {
+                    label: "Admin",
+                    value: "admin",
+                },
+                {
+                    label: "User",
+                    value: "user",
+                },
+            ]
+
+        }
+    ]
+}
