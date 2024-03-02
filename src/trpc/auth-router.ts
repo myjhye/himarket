3126bdc3a5f4cf2be6549540ca981685d77b3239
myjhye <t01087633799@gmail.com>
2024-03-02 17:33:06 +0900

    comment - auth-router

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -1,17 +1,22 @@
+// 회원가입 백엔드 - 입력 값 유효성 검사, 중복 사용자 검사, 사용자 생성(데이터베이스 추가), 응답 반환
+
 import { AuthCredentialsValidator } from '../lib/validators/account-credentials-validator'
 import { publicProcedure, router } from './trpc'
 import { getPayloadClient } from '../get-payload'
 import { TRPCError } from '@trpc/server'
-import { z } from 'zod'
 
 export const authRouter = router({
   createPayloadUser: publicProcedure
+    // 사용자 입력 값 유효성 검사
     .input(AuthCredentialsValidator)
+    // 사용자 입력 값 기반으로 새 사용자 생성(회원가입)
     .mutation(async ({ input }) => {
+      // 회원가입 입력 값
       const { email, password } = input
+      // payload 인스턴스
       const payload = await getPayloadClient()
 
-      // check if user already exists
+      // 중복 사용자 데이터베이스에서 검사 - 이메일 
       const { docs: users } = await payload.find({
         collection: 'users',
         where: {
@@ -21,9 +26,11 @@ export const authRouter = router({
         },
       })
 
+      // 같은 이메일 가진 사용자 있으면 에러
       if (users.length !== 0)
         throw new TRPCError({ code: 'CONFLICT' })
 
+      // 새 사용자 생성
       await payload.create({
         collection: 'users',
         data: {
@@ -33,6 +40,7 @@ export const authRouter = router({
         },
       })
 
+      // 사용자 생성 성공 - 응답
       return { 
         success: true, 
         sentToEmail: email 
