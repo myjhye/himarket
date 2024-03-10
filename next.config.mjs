371bcdbc34abc9f01dba0171b3426e8cc76424ac
myjhye <t01087633799@gmail.com>
2024-03-10 11:59:32 +0900

    Next.js 설정 - 로컬 서버에 저장된 이미지를 사용 가능하게

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+// Next.js 설정
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // 로컬 서버에 저장된 이미지를 사용 가능하게
+    images: {
+        remotePatterns: [
+            {
+                hostname: "localhost",
+                pathname: "**",
+                port: "3000",
+                protocol: "http",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
