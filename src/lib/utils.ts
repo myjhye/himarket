03542d277a56140ab3922e2bb3ab5f230fb1d000
myjhye <t01087633799@gmail.com>
2024-03-24 18:10:42 +0900

    결제 화폐 달러로 변경

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -8,25 +8,19 @@ export function cn(...inputs: ClassValue[]) {
 export function formatPrice(
   price: number | string,
   options: {
-    currency?: "USD" | "EUR" | "GBP" | "BDT" | "KRW",
-    notation?: Intl.NumberFormatOptions["notation"]
+    currency?: 'USD' | 'EUR' | 'GBP' | 'BDT'
+    notation?: Intl.NumberFormatOptions['notation']
   } = {}
 ) {
+  const { currency = 'USD', notation = 'compact' } = options
 
-  // 기본으로 'USD', 'compact' 사용
-  const { 
-    currency = "KRW",  
-    notation = "standard" 
-  } = options
+  const numericPrice =
+    typeof price === 'string' ? parseFloat(price) : price
 
-  // price가 문자열이면 숫자로 변환
-  const numericPrice = typeof price === "string" ? parseFloat(price) : price
-
-  // 제공된 옵션에 따라 숫자 가격 형식화 - Intl.NumberFormat 사용
-  return new Intl.NumberFormat("ko-KR", {
-    style: "currency",
-    currency: currency,
-    notation: notation,
-    maximumFractionDigits: 0,
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2,
   }).format(numericPrice)
 }
